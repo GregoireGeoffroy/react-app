5c120d7c2d91da94460974362b3496d3f023b1ff
GregoireGeoffroy <thegathering92@gmail.com>
2024-08-16 16:53:06 +0300

    say hello

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Greeting from './greetings.jsx'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Greeting />
   </StrictMode>,
 )
